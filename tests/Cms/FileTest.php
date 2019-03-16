b130f64ab6b82eb715bb179914831892f6e6972e
Nico Hoffmann <on-github@nhoffmann.com>
2019-03-16 09:49:51 +0100

    Fix File::modified #1559

---
+++ tests/Cms/FileTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Kirby\Cms;
+
+class FileTest extends TestCase
+{
+    protected $app;
+    protected $fixtures;
+
+    public function app()
+    {
+        return new App([
+            'roots' => [
+               'index' => $this->fixtures = __DIR__ . '/fixtures/files'
+            ],
+            'site' => [
+                'children' => [
+                    [
+                        'slug'  => 'test',
+                        'files' => [
+                            [
+                                'filename' => 'test.jpg'
+                            ]
+                        ]
+                    ]
+                ]
+            ]
+        ]);
+    }
+
+
+    public function testModified()
+    {
+        $app = $this->app();
+        $file = $app->page('test')->file('test.jpg');
+
+        $this->assertInstanceOf(File::class, $file);
+        $this->assertEquals(1552725866, $file->modified());
+    }
+}
